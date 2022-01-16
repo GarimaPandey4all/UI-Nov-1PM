10b2f3407e8546a1a3ef51d37b800949e614dfc8
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-01-16 12:59:15 +0530

    Add files via upload

---
+++ Javascript/BasicSyntax/Basic.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', initEvents);
+
+function initEvents()
+{
+    //event binding
+    document.getElementById("btn").addEventListener('click', showName);
+}
+
+// window.addEventListener('load', function(){
+//     //event binding
+//     document.getElementById("btn").addEventListener('click', showName);
+// })
+
+//event binding
+//document.getElementById("btn").addEventListener('click', showName);
+
+function showName()
+{
+    var username = document.getElementById("box_1").value;
+    // document.getElementById("result").innerHTML += username;
+
+    document.getElementById("result").innerHTML = username;
+}
